248579c2b8b3ad757e33501ad01a4c477b2ac8b9
Patrick Taylor <patrick.andrew.taylor@gmail.com>
2018-07-24 12:06:17 -0400

    mastering-pycharm-hilo-1 Write a quick game that will generate the output below.

--- game.py
+++ game.py
@@ -1,8 +1,23 @@
 import random
 
+number = random.randint(1, 100)
+
+
 def main():
 
     print("Welcome to the HI - LO game")
+
+    guess = 0
+
+    while guess != number:
+        guess = int(input("Guess a number between 1 & 100: "))
+        if guess < number:
+            print("Too low!")
+        elif guess > number:
+            print("Too high!")
+
+    print("Got it: The number is {}".format(number))
+
 
 if __name__ == '__main__':
     main()
